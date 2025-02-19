bd021151f1d6d0d3e21f05167449987f2093f7fa
Lousiboupi <louissicot17@gmail.com>
2025-02-19 03:54:38 +0100

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -212,6 +212,7 @@ function ProductList() {
             ]
         }
     ];
+    
    const styleObj={
     backgroundColor: '#4CAF50',
     color: '#fff!important',
@@ -268,6 +269,21 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+                {plantsArray.map((category, index) => (
+    <div key={index}>
+        <h1><div>{category.category}</div></h1>
+        <div className="product-list">
+            {category.plants.map((plant, plantIndex) => (
+            <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
+                {/*Similarly like the above plant.name show other details like description and cost*/}
+                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+            </div>
+            ))}
+        </div>
+    </div>
+    ))}
 
 
         </div>
@@ -277,5 +293,12 @@ const handlePlantsClick = (e) => {
     </div>
     );
 }
-
+const [addedToCart, setAddedToCart] = useState({});
+const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+     }));
+  };
 export default ProductList;
